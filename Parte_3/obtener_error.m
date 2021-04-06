ea7f6b8750059380724de9fbafaf2936f7a163d6
Edgar Chaves <edjchg@gmail.com>
2021-04-06 09:10:00 -0600

    Fix

--- Parte_3/obtener_error.m
+++
@@ -1,8 +0,0 @@
-function error = obtener_error(A, L_t, S_t)
-  %La funcion obtener_error toma, 3 matrices y calcula
-  %con ellas el error entre ellas, mediante la norma de 
-  %Frobenius.
-  %Entradas: matrices de tamanno mxn A, L_t, S_t del algoritmo godec.
-  %Salida: el error obtenido.
-  error = (norm(A - L_t - S_t, 'fro')**2)/(norm(A, 'fro')**2);
-endfunction
