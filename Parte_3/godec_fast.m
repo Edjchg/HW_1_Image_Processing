2e0634c9cd28c8ac2580df915f195bf4139bbbd2
ch99AM <christianalpizar99@gmail.com>
2021-04-06 14:58:45 -0600

    Otro forma de calcular p_c0

--- Parte_3/godec_fast.m
+++ Parte_3/godec_fast.m
@@ -10,7 +10,7 @@ function [L, S] = godec_fast(A, k, c_0, epsilon)
     %[U, E, V] = svd(A - S_t);
     %L_t = U(:,1:k)*E(1:k,1:k)*V(:,1:k)'; 
     L_t = rango_reducido_bilateral(A-S_t, k, 3);
-    S_t = P_c0(A - L_t, c_0);
+    S_t = P_c02(A - L_t, c_0);
     if abs(obtener_error(A, L_t, S_t) - E_t) < epsilon
     %if contador == 0
       break
--- Parte_3/using_godec.m
+++ Parte_3/using_godec.m
@@ -7,4 +7,4 @@ c_0 = 8;
 epsilon = 0.0001;
 
 %[L, S, error] = godec(A, k, c_0, epsilon)
-[L, S, error] = godec_fast(A, k, c_0, epsilon)
+[L, S, error] = godec(A, k, c_0, epsilon)
