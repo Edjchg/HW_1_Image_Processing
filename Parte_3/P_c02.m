54b253e7aaa94034fc38b4fef418e1952436b0fe
Edgar Chaves <edjchg@gmail.com>
2021-04-06 23:52:18 -0600

    Add optimization finding max of a matrix

--- Parte_3/P_c02.m
+++ Parte_3/P_c02.m
@@ -5,8 +5,8 @@ function S_t = P_c02(matriz, c)
   i = m;j = n; contador = 1;
   S_t = zeros(m,n);
   for i = 1: c
-    max_valor = max(max(matriz_copia));
-    [x y] = find(max_valor == matriz_copia);
+    [max_valor x] = max(matriz_copia);
+    [max_valor y] = max(max_valor);
     S_t(x, y) = matriz(x, y);
     matriz_copia(x, y) = 0;
   endfor  
